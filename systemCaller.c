fd39e3fe92bd127c98f940e39179bd6cc81a7532
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2023-02-03 12:23:39 -0800

    added includes

---
+++ systemCaller.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h> 
+#include <sys/wait.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <fcntl.h> 
+#include <errno.h> 
+#include <sys/stat.h> 
+
