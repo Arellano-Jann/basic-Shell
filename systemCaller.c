696f5eaa3938057c53464dd2adc0856c1c3c7235
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2023-02-03 12:36:19 -0800

    inputted stubs

--- systemCaller.c
+++ systemCaller.c
@@ -6,5 +6,25 @@
 #include <unistd.h> 
 #include <fcntl.h> 
 #include <errno.h> 
-#include <sys/stat.h> 
+#include <sys/stat.h>
 
+int main(){
+
+}
+
+int executeCommand(char* []){
+
+}
+
+void changeDirectories(char*){
+
+}
+
+int parseInput(char *input, char *splitWords[]){
+    int wordInd = 0;
+    splitWords[0] = strtok(input, " \n");
+    while(splitWords[wordInd] != NULL){
+        splitWords[++wordInd] = strtok(NULL, " \n");
+    }
+    return wordInd;
+}
