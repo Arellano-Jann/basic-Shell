d2ccf9c321a120956e4acfde6d18ea1283340b8d
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-02-13 15:28:42 -0800

    finished executeCommand?

--- systemCaller.c
+++ systemCaller.c
@@ -8,9 +8,35 @@
 #include <errno.h> 
 #include <sys/stat.h>
 
+// 20
+int executeCommand(char* cmd[]){
+    pid_t pid = fork();
 
-int executeCommand(char* []);
-void changeDirectories(char*);
+    if (pid == -1){ // if unsuccessful
+        printf("Error in creating child process!");
+        return 1;
+    }
+    
+    if (pid == 0){ // Child successfully created
+        if (execvp(cmd[0], cmd) == -1){ // if unsuccessful. code has side effects
+            printf("Command Not Found!");
+            return 1;
+        }
+    }
+    else if (waitpid(pid, NULL, 0) == -1){// can also be waitpid(pid, &status, 0) // if unsuccessful
+        printf("Error in waiting for child process!");
+        return 1;
+    }
+    return 0;
+}
+
+// 5
+void changeDirectories(char* path){
+    if (chdir(path) != 0){
+        printf("Path Not Found!");
+    }
+}
+
 int parseInput(char *input, char *splitWords[]);
 
 // 30
@@ -20,17 +46,6 @@ int main(){
     }
 }
 
-// 20
-int executeCommand(char* []){
-
-}
-
-// 5
-void changeDirectories(char*){
-    if (chdir(char*) != 0){
-        printf("Path Not Found!")
-    }
-}
 
 int parseInput(char *input, char *splitWords[]){
     int wordInd = 0;
